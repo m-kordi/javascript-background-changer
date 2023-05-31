13e90b03fdc0020925b5c65313b90087206b6274
m-kordi <mh.alkordi@gmail.com>
2023-06-01 02:32:13 +0300

    done

--- src/components/resumec/resumeimg/Resumeimg.jsx
+++ src/components/resumec/resumeimg/Resumeimg.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import "./Resumeimg.css"
 import myresume from "../../../assets/Mohammad_Alkordi_Resume.jpg"
+import loading from "../../../assets/loading.gif"
 
 function Resumeimg() {
   return (
     <div className='resumejpg-img'>
-        <img src={myresume} alt="#" style={{background:"white"}} />
+        <img src={myresume} loading='lazy' alt={loading} style={{background:"white"}} />
     </div>
   )
 }
